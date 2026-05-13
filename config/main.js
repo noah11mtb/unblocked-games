682feafc47413b1e6851384a4f2622e20c590021
Noah <noah11mtb@gmail.com>
2026-05-13 17:31:27 +1000

    Update site name and subtext in main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "Unblocked Craze Games."; // Change this to change the name of your website.
+var subtext = "v2"; // set the subtext
 
 // more settings in main.css
 
